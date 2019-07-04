d8e95f68c6bb708e11613772c61f8f58cb89c378
Didier Matos <d.matostorres@gmail.com>
2019-07-04 13:06:55 -0500

    Agregando dos botones de siguiente y atras junto con todas las posibilidades de su funcionamiento es decir que desapareceran o apareceran o saldra el boton finalizar

--- app/src/main/java/com/ndevelopers/onboardingapp/MainActivity.java
+++ app/src/main/java/com/ndevelopers/onboardingapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
     private SliderAdapter sliderAdapter;
 
+    private Button mNextBtn;
+    private Button mBackBtn;
+
+    private int mCurrentPage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
         mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
 
+        mNextBtn = findViewById(R.id.nextBtn);
+        mBackBtn = findViewById(R.id.prevBtn);
+
         sliderAdapter = new SliderAdapter(this);
 
         mSlideViewPager.setAdapter(sliderAdapter);
@@ -31,6 +41,20 @@ public class MainActivity extends AppCompatActivity {
         addDotsIndicator(0);
 
         mSlideViewPager.addOnPageChangeListener(viewListener);
+
+        mNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSlideViewPager.setCurrentItem(mCurrentPage + 1);
+            }
+        });
+
+        mBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSlideViewPager.setCurrentItem(mCurrentPage - 1);
+            }
+        });
 
     }
 
@@ -43,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
             mDots[i] = new TextView(this);
             mDots[i].setText(Html.fromHtml("&#8226"));
-            mDots[i].setTextSize(50);
+            mDots[i].setTextSize(35);
             mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
 
             mDotLayout.addView(mDots[i]);
@@ -63,6 +87,31 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onPageSelected(int i) {
             addDotsIndicator(i);
+            mCurrentPage = i;
+
+            if(i==0){
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(false);
+                mBackBtn.setVisibility(View.INVISIBLE);
+
+                mNextBtn.setText("SIGUIENTE");
+                mBackBtn.setText("");
+            } else
+                if(i== mDots.length - 1){
+                    mNextBtn.setEnabled(true);
+                    mBackBtn.setEnabled(true);
+                    mBackBtn.setVisibility(View.VISIBLE);
+
+                    mNextBtn.setText("FINALIZAR");
+                    mBackBtn.setText("REGRESAR");
+                }else{
+                    mNextBtn.setEnabled(true);
+                    mBackBtn.setEnabled(true);
+                    mBackBtn.setVisibility(View.VISIBLE);
+
+                    mNextBtn.setText("SIGUIENTE");
+                    mBackBtn.setText("REGRESAR");
+                }
         }
 
         @Override
